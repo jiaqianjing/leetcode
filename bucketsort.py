dc6dc0333109f74cf6b8b0acb7cede069214535e
jiaqianjing <jiaqianjing@cola-2.local>
2020-04-14 14:06:05 +0800

    modify bucket sort

--- bucketsort.py
+++ bucketsort.py
@@ -4,19 +4,21 @@ version:
 Author: jiaqianjing
 Date: 2020-03-01 10:54:30
 LastEditors: jiaqianjing
-LastEditTime: 2020-04-10 12:44:17
+LastEditTime: 2020-04-14 14:05:12
 """
 class Solution:
     def bucket_sort(self, nums):
+        if not nums:
+            return []
         bucket = [0] * 11
         for i in nums:
             bucket[i] += 1
-        result = []
+        res = []
         for index, val in enumerate(bucket):
-            if val > 0:
-                for k in range(val):
-                    result.append(index)
-        return result
+            if val >= 1:
+                for _ in range(val):
+                    res.append(index)
+        return res
 
         
 a = [2,3,6,1,2,2]
@@ -25,5 +27,3 @@ s = Solution()
 res = s.bucket_sort(a)
 print("bucket sort: {}".format(res))
 
-
-            
